d469bb59d13da7f835a8a0f4931c5cc1e38d06c4
AhmedFalahQ <ahmed.falah.alqahtani@gmail.com>
2025-06-14 00:02:00 +0300

    Version2

--- utils/aws_utils.py
+++ utils/aws_utils.py
@@ -1,18 +1,27 @@
 import boto3
 import base64
-from botocore.exceptions import ClientError
 import json
 import uuid
+import io
+from PIL import Image
+
 def invoke_lambda_to_store_image(image_bytes, bucket_name, lambda_function_name, region="us-east-1"):
-    """Invoke AWS Lambda to store the uploaded image in S3 for future training."""
+
+    """Invoke AWS Lambda to store the uploaded image in S3."""
     client = boto3.client("lambda", region_name=region)
-
-    # Create a unique filename
-    filename = f"uploads/{uuid.uuid4()}.jpg"
-
-    # Encode image to base64 to safely pass in payload
-    encoded_image = base64.b64encode(image_bytes).decode("utf-8")
-
+    try:
+        img = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+        img_resized = img.resize((224, 224))
+        buffer = io.BytesIO()
+        img_resized.save(buffer, format="JPEG", quality=90)
+        buffer.seek(0)
+        resized_bytes = buffer.read()
+        encoded_image = base64.b64encode(resized_bytes).decode("utf-8")
+    except Exception as e:
+        print(f"❌ Failed to process image: {e}")
+        return
+    
+    filename = f"uploads/{uuid.uuid4()}.jpg" # Create a unique filename
     payload = {
         "filename": filename,
         "image_data": encoded_image,
